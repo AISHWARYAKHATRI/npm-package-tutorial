94cf61e148cf9b9fc001bf8082decddaecc26ae4
AISHWARYAKHATRI <191200107021@asoit.edu.in>
2024-04-20 12:00:30 +0530

    initial

---
+++ src/components/Button.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { ReactNode } from "react";
+
+type PropType = {
+  children: ReactNode;
+};
+
+const Button = ({ children }: PropType) => {
+  return (
+    <button
+      style={{
+        padding: "10px 20px",
+        fontSize: "1.2rem",
+        borderRadius: "5px",
+        cursor: "pointer",
+        backgroundColor: "blue",
+        color: "white",
+        border: "none",
+      }}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
